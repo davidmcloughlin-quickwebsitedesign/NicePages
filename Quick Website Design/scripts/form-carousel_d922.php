45b4a876e59210e6ddc15bb79b033785de12d44a
davidmcloughlin-quickwebsitedesign <67553263+davidmcloughlin-quickwebsitedesign@users.noreply.github.com>
2021-09-01 16:44:57 +0200

    New QWD Website

---
+++ Quick Website Design/scripts/form-carousel_d922.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'Website@quickwebsitedesign.ie',
+    'to' => 'david.mcloughlin@quickwebsitedesign.ie'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'email',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'text' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'text',
+    'required' => false,
+    'errors' => array(
+    'required' => 'Field \'text\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 4,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
